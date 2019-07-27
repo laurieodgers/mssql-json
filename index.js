677dd887a74d09fc3f5de6f03a398ab2a64c8b99
Laurie Odgers <laurieodgers83@gmail.com>
2019-07-27 23:42:50 +1000

    better constraints

--- index.js
+++ index.js
@@ -83,62 +83,110 @@ module.exports = class MssqlJson {
             // set description as the col type
             properties[columnName].description = dataType
 
+            var dataTypeOnly = dataType.replace(/\(.*\)/, '')
+
+            var isString = false
             // useful: https://www.connectionstrings.com/sql-server-2008-data-types-reference/
-            switch (dataType.replace(/\(.*\)/, '')) {
-                case 'CHAR':
-                case "NCHAR":
-                case 'VARCHAR':
-                case 'NVARCHAR':
-                case "TEXT":
-                case "NTEXT":
-                    properties[columnName].type = 'string'
-
-                    // extract the max length
-                    var maxLength = dataType.substring(
-                        dataType.lastIndexOf("(") + 1,
-                        dataType.lastIndexOf(")")
-                    );
-
-                    properties[columnName].maxlength = maxLength
+            switch (dataTypeOnly.toUpperCase()) {
+                case 'BIGINT':
+                    properties[columnName].type = 'integer'
+                    properties[columnName].minimum = Math.pow(-2, 63)
+                    properties[columnName].exclusiveMaximum = Math.pow(2, 63)
                     break
                 case 'INT':
-                case 'BIGINT':
-                case 'SMALLINT':
-                case 'TINYINT':
-                case "XML":
                     properties[columnName].type = 'integer'
+                    properties[columnName].minimum = -2147483648
+                    properties[columnName].maximum = 2147483647
+                    break
+                case 'SMALLINT':
+                    properties[columnName].type = 'integer'
+                    properties[columnName].minimum = -32768
+                    properties[columnName].maximum = 32767
+                    break
+                case 'TINYINT':
+                    properties[columnName].type = 'integer'
+                    properties[columnName].minimum = 0
+                    properties[columnName].maximum = 255
                     break
                 case "BIT":
                     properties[columnName].type = 'boolean'
                     break
                 case "DECIMAL":
                 case "NUMERIC":
-                case "MONEY":
-                case "SMALLMONEY":
-                case "FLOAT":
-                case "REAL:":
                     properties[columnName].type = 'number'
+                    properties[columnName].exclusiveMinimum = Math.pow(-10, 38)
+                    properties[columnName].exclusiveMaximum = Math.pow(10, 38)
+                    break
+                case "MONEY":
+                    properties[columnName].type = 'number'
+                    properties[columnName].minimum = Math.pow(-2, 63) / 10000
+                    properties[columnName].maximum = (Math.pow(2, 63) - 1) / 10000
+                    break
+                case "SMALLMONEY":
+                    properties[columnName].type = 'number'
+                    properties[columnName].minimum = -214748.3648
+                    properties[columnName].maximum = 214748.3647
+                    break
+                case "FLOAT":
+                    properties[columnName].type = 'number'
+                    properties[columnName].minimum = -1.79e308
+                    properties[columnName].maximum = 1.79e308
+                    break
+                case "REAL":
+                    properties[columnName].type = 'number'
+                    properties[columnName].minimum = -3.40e38
+                    properties[columnName].maximum = 3.40e38
                     break
                 case "DATETIME":
                 case "SMALLDATETIME":
                 case "DATETIME2":
                     properties[columnName].type = 'string'
+                    // TODO should we accept sql server syntax instead?
                     properties[columnName].format = "date-time"
                     break
                 case "DATE":
                     properties[columnName].type = 'string'
+                    // TODO should we accept sql server syntax instead?
                     properties[columnName].format = "date"
                     break
                 case "TIME":
                     properties[columnName].type = 'string'
+                    // TODO should we accept sql server syntax instead?
                     properties[columnName].format = "time"
                     break
-
+                case 'CHAR':
+                    isString = true
+                    properties[columnName].maxLength = 8000
+                    break
+                case "NCHAR":
+                    isString = true
+                    properties[columnName].maxLength = 8000
+                    break
+                case 'VARCHAR':
+                    isString = true
+                    properties[columnName].maxLength = Math.pow(2, 31)
+                    break
+                case 'NVARCHAR':
+                    isString = true
+                    properties[columnName].maxLength = Math.pow(2, 30)
+                    break
+                case "TEXT":
+                    isString = true
+                    properties[columnName].maxLength = 2147483647
+                    break
+                case "NTEXT":
+                    isString = true
+                    properties[columnName].maxLength = 1073741823
+                    break
                 case "UNIQUEIDENTIFIER":
                     properties[columnName].type = 'string'
                     properties[columnName].format = "uuid"
-                    properties[columnName].maxlength = "36"
+                    properties[columnName].minLength = 36
+                    properties[columnName].maxLength = 36
                     break;
+                case "XML":
+                    properties[columnName].type = 'string'
+                    break
                 // unsupported yet
                 //case "DATETIMEOFFSET":
                 //case "BINARY":
@@ -149,6 +197,23 @@ module.exports = class MssqlJson {
                 //case "HIERARCHYID":
                 default:
                     throw ('Unknown type: ' + dataType)
+            }
+
+            // handle extra stuff for strings
+            if (isString) {
+                properties[columnName].type = 'string'
+                properties[columnName].minLength = 0
+
+                // extract the max length
+                var maxLength = dataType.substring(
+                    dataType.lastIndexOf("(") + 1,
+                    dataType.lastIndexOf(")")
+                );
+
+                // if this isnt set to max then include it (max was set above)
+                if (maxLength.toLowerCase() != 'max') {
+                    properties[columnName].maxLength = parseInt(maxLength)
+                }
             }
 
             // check if it can be null
